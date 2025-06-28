9097a206c27f8bc581624e966f7d0ca9fa5d2f95
HammadAli64 <hadich5066@gmail.com>
2025-06-28 16:20:03 +0500

    Initial project upload

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,31 @@
+
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://codder.pythonanywhere.com/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
